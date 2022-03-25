c310361cd94d349ec58408ba723108e4bfa7c4d9
haseebDotJs <haseeb.khalid03452800681@gmail.com>
2022-03-25 22:09:04 +0500

    sub blog page contentful integration (partial)

--- src/template/post-contentful.js
+++ src/template/post-contentful.js
@@ -1,42 +1,59 @@
-import { graphql } from 'gatsby'
-import React from 'react'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { graphql } from "gatsby"
+import React from "react"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 const postContentful = ({ data }) => {
-    const { image, content, title } = data.contentfulPost
-    const document = JSON.parse(content.raw)
-    return (
-        <div>
-            <header style={{ height: "50px", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "red" }}>
-                Header
-            </header>
-            <h1>{title}</h1>
-            <div>
-                <img src={image.file.url} alt={title} />
-            </div>
-            {documentToReactComponents(document)}
-            <footer style={{ height: "50px", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "orange" }}>
-                footer
-            </footer>                 
-        </div>
-    )
+  const { image, content, title } = data.contentfulPost
+  // const document = JSON.parse(content.raw)
+  return (
+    <div>
+      <header
+        style={{
+          height: "50px",
+          textAlign: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "red",
+        }}
+      >
+        Header
+      </header>
+      <h1>{title}</h1>
+      <div>
+        <img src={image.file.url} alt={title} />
+      </div>
+      {documentToReactComponents(document)}
+      <footer
+        style={{
+          height: "50px",
+          textAlign: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "orange",
+        }}
+      >
+        footer
+      </footer>
+    </div>
+  )
 }
 
-export const query = graphql` 
-    query Post($slug: String) {
-        contentfulPost(slug: {eq: $slug}) {
-            slug
-            title
-            image {
-                file {
-                    url
-                }
-            }
-            content {
-                raw
-            }
+export const query = graphql`
+  query Post($slug: String) {
+    contentfulPost(slug: { eq: $slug }) {
+      slug
+      title
+      image {
+        file {
+          url
         }
+      }
+      content {
+        raw
+      }
     }
-  
+  }
 `
 export default postContentful
